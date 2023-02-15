4d9a66f1a7e55ff79affe41a71f5a5eb0059169d
BrentHickman <BrentHickman@ymail.com>
2023-02-15 13:00:25 -0800

    api calls work and returns array of 3 hotels near location code with available checkin/checkout dates.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import Triangle from './triangle.js';
-function handleTriangleForm() {
+import Hotels from './js/hotel.js';
+
+
+function handleFormSubmission(event) {
   event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+  const airportCode = document.querySelector('#airportCodeInput').value;
+  document.querySelector('#airportCodeInput').value = null;
+  const checkIn = document.querySelector('#checkInInput').value;
+  const checkOut = document.querySelector('#checkOutInput').value;
+  let myHotels = new Hotels();
+  myHotels.getLocationId(airportCode, checkIn, checkOut);
 }
 
 window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
---
+++ src/js/hotel.js
@@ -0,0 +1,80 @@
+
+export default class Hotels {
+  constructor() {
+    this.foundHotels = [];
+
+  }
+  async locateHotel(airportCode) {
+    return new Promise(function (resolve, reject) {
+      let locationRequest = new XMLHttpRequest();
+      const hotelLocationUrl = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/locations?search_type=AIRPORT&name=${airportCode}`;
+      locationRequest.addEventListener("loadend", function () {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, airportCode]);
+        } else {
+          reject([this, response]);
+        }
+      });
+      locationRequest.open("GET", hotelLocationUrl, true);
+      locationRequest.setRequestHeader('X-RapidAPI-Key', process.env.HOTEL_API_KEY);
+      locationRequest.send();
+    });
+  }
+
+  getLocationId(airportCode, checkIn, checkOut) {
+    this.locateHotel(airportCode).then(ele => {
+      let foundCityID = ele[0][0].cityID;
+      console.log(ele);
+      this.parseHotels(foundCityID, checkIn, checkOut);
+    },
+    function (errorArray) {
+      this.printError(errorArray);
+    });
+  }
+
+  async searchHotels(locationID, checkIn, checkOut) {
+    return new Promise(function (resolve, reject) {
+      let searchRequest = new XMLHttpRequest();
+      const hotelSearchUrl = `https://priceline-com-provider.p.rapidapi.com/v1/hotels/search?date_checkout=${checkOut}&sort_order=PROXIMITY&date_checkin=${checkIn}&location_id=${locationID}&star_rating_ids=3.0,3.5,4.0,4.5,5.0`;
+      searchRequest.addEventListener("loadend", function () {
+        console.log(this);
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, locationID]);
+        } else {
+          reject([this, response]);
+        }
+      });
+      searchRequest.open("GET", hotelSearchUrl, true);
+      searchRequest.setRequestHeader('X-RapidAPI-Key', process.env.HOTEL_API_KEY);
+      searchRequest.send();
+    });
+
+  }
+
+  parseHotels(locationID, checkIn, checkOut) {
+    this.searchHotels(locationID, checkIn, checkOut).then(ele => {
+      let foundHotelsArray = ele[0].hotels;
+      console.log(foundHotelsArray);
+      let counter = 0;
+      while (this.foundHotels.length < 3) {
+        if (foundHotelsArray[counter].name) {
+          let addressString = foundHotelsArray[counter].location.address.addressLine1 + " " + foundHotelsArray[counter].location.address.cityName + " " + foundHotelsArray[counter].location.address.provinceCode + " " + foundHotelsArray[counter].location.address.zip;
+          this.foundHotels.push([foundHotelsArray[counter].name, foundHotelsArray[counter].ratesSummary.minPrice, addressString, foundHotelsArray[counter].media.url]);
+        }
+        counter++;
+      }
+      console.log(this.foundHotels);
+    },
+    function (errorArray) {
+      this.printError(errorArray);
+    });
+  }
+  printError(error) {
+    document.querySelector('#response').setAttribute("class", "error");
+    document.querySelector('#response').innerText = `There was an error accessing the exchange data: ${error[0].status}: ${error[0].response}`;
+  }
+
+}
+
