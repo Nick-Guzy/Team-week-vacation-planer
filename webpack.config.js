48bab51729fe1359dca8f6d53d019de059fe7e81
BrentHickman <BrentHickman@ymail.com>
2023-02-16 08:34:03 -0800

    updated title and pathways on webpack and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Weekend Getaway Planner',
       template: './src/index.html',
       inject: 'body'
     })
